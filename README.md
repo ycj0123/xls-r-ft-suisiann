627f2ebd929ec269d5d170b4879749ed5e7eb7ba
ycj0123 <t20508@gmail.com>
2022-10-30 23:44:30 +0800

    add README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # xls-r-ft-suisiann
-Fine-tuning XLS-R on the SuíSiann Dataset
+Fine-tuning XLS-R on the SuíSiann Dataset.
+
+```bash=
+# generate tokenizer
+python tokenizer.py
+
+# train the model
+python fine_tune_xls_r.py
+
+# evaluate
+python eval.py
+
+# inference
+python inference.py
+
+# open a gradio web app
+python gradio_run.py
+
+# push the fine_tuned model to HuggingFace model hub
+python push_to_hub.py
+```
--- push_to_hub.py
+++ push_to_hub.py
@@ -2,8 +2,9 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 
 
 # load model and processor
-model = Wav2Vec2ForCTC.from_pretrained('/home/ycj0123/xlsr/wav2vec2-large-xls-r-300m-taigi/checkpoint-1200').to("cuda")
-processor = Wav2Vec2Processor.from_pretrained('/home/ycj0123/xlsr/wav2vec2-large-xls-r-300m-taigi/checkpoint-1200', unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
+model_name = './wav2vec2-large-xls-r-300m-taigi-test/checkpoint-3000'
+model = Wav2Vec2ForCTC.from_pretrained(model_name).to("cuda")
+processor = Wav2Vec2Processor.from_pretrained(model_name, unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
 
 # upload tokenizer to hf repo
 repo_name = "wav2vec2-large-xls-r-300m-taigi"
